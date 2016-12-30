c9555da8d21b216459f3ceac0c6d8320880d91af
gexciter <rchilson@gmail.com>
2016-12-30 17:28:12 -0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1483134771,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/truck-sheet0.png",
+		"images/pants-sheet0.png",
+		"images/taco-sheet0.png",
+		"images/explosion-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
